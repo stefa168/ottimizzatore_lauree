efaae878fd725b30d64707c6fb250f9bde96cede
Stefano Vittorio Porta <stefa168@hotmail.it>
2024-02-27 23:01:50 +0100

    feat: added tabs

--- web/src/routes/commission/[id]/+page.svelte
+++ web/src/routes/commission/[id]/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import * as Tabs from "$lib/components/ui/tabs";
     import DataTable from './commission-table.svelte'
     export let data;
 
@@ -8,5 +9,23 @@
 
 <div class="container mx-auto py-10">
     <h1 class="text-2xl mb-4">{commission.title}</h1>
-    <DataTable {commission}/>
+    <Tabs.Root value="candidates">
+        <div id="toolbar" class="w-full">
+            <Tabs.List class="content-center">
+                <Tabs.Trigger value="candidates">Candidati</Tabs.Trigger>
+                <Tabs.Trigger value="professors">Docenti</Tabs.Trigger>
+                <Tabs.Trigger value="optimization">Ottimizzazione</Tabs.Trigger>
+            </Tabs.List>
+        </div>
+        <Tabs.Content value="candidates">
+            <DataTable {commission}/>
+        </Tabs.Content>
+        <Tabs.Content value="professors" class="text-center">
+            Change your password here.
+        </Tabs.Content>
+        <Tabs.Content value="optimization" class="text-center">
+            Ottimizzazione
+        </Tabs.Content>
+    </Tabs.Root>
+
 </div>
