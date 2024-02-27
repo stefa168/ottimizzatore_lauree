7bc0219d93563e6996327802563d02a3e0bce94e
Stefano Vittorio Porta <stefa168@hotmail.it>
2024-02-27 18:18:01 +0100

    chore: removed unused file

--- web/src/routes/+page.ts
+++
@@ -1,8 +0,0 @@
-import {superValidate} from "sveltekit-superforms";
-import {zod} from "sveltekit-superforms/adapters";
-import {commissionFormSchema} from "./schema";
-
-export const load = (async () => {
-    const form = await superValidate(zod(commissionFormSchema));
-    return {form};
-});
