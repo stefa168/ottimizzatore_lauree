15d336e5f52f6cf45a598b09b86f3c2a439722ba
Stefano Vittorio Porta <stefa168@hotmail.it>
2024-02-27 20:13:57 +0100

    chore: removed testing delay

--- web/src/routes/commission/[id]/+page.ts
+++ web/src/routes/commission/[id]/+page.ts
@@ -1,14 +1,8 @@
 import type {Commission} from "./commission_types";
 
-const delay = function (ms: number) {
-    return new Promise(res => setTimeout(res, ms));
-};
-
 export async function load({fetch, params}) {
     let commission: Commission = await fetch(`http://localhost:5000/commission/${params.id}`)
         .then(response => response.json())
-
-    await delay(3000);
 
     return {
         commissionId: params.id,
