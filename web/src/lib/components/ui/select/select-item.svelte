e9ed899c0896ba2bf0a95550e35db93d41415d14
Stefano Vittorio Porta <stefa168@hotmail.it>
2024-02-29 21:59:45 +0100

    fix: fixed icons import

--- web/src/lib/components/ui/select/select-item.svelte
+++ web/src/lib/components/ui/select/select-item.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { cn } from "$lib/utils";
 	import { Select as SelectPrimitive } from "bits-ui";
-	import Check from "svelte-radix/Check.svelte";
+	import RadixIconsCheck from '~icons/radix-icons/check'
 
 	type $$Props = SelectPrimitive.ItemProps;
 	type $$Events = Required<SelectPrimitive.ItemEvents>;
@@ -28,7 +28,7 @@
 >
 	<span class="absolute right-2 flex h-3.5 w-3.5 items-center justify-center">
 		<SelectPrimitive.ItemIndicator>
-			<Check class="h-4 w-4" />
+			<RadixIconsCheck class="h-4 w-4" />
 		</SelectPrimitive.ItemIndicator>
 	</span>
 	<slot>
