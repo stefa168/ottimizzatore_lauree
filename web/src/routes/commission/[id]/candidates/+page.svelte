7b8b0e903e71481e7246233cc730635df310f364
Stefano Vittorio Porta <stefa168@hotmail.it>
2024-03-20 02:54:10 +0100

    fix(commission): fixed students table

--- web/src/routes/commission/[id]/candidates/+page.svelte
+++ web/src/routes/commission/[id]/candidates/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
     import StudentsTable from '../commission-students-table.svelte';
-    import type {Commission} from "../commission_types";
-
-    export let data: { commissionId: string, commissionData: Commission };
-
-    $: commission = data.commissionData
+    import {selectedProblem} from "$lib/store";
 </script>
 
-<StudentsTable {commission}/>
+{#if $selectedProblem !== undefined}
+    <StudentsTable commission={$selectedProblem}/>
+{/if}
