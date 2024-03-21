4e804721509cc3aea502decb657cd8d81cf9deeb
Stefano Vittorio Porta <stefa168@hotmail.it>
2024-03-21 01:06:41 +0100

    fix(commission): added custom error page for simple errors at the commission level

---
+++ web/src/routes/commission/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import MdiRobotDeadOutline from '~icons/mdi/robot-dead-outline'
+</script>
+
+<div class="mt-32 flex flex-col items-center justify-center h-full">
+    <MdiRobotDeadOutline class="w-16 h-16"/>
+    <div class="mt-4 text-center text-lg font-medium text-gray-600 dark:text-gray-400">
+        <p>Si è verificato un errore!</p>
+        <p class="font-mono">{$page?.status}: {$page?.error?.message}</p>
+    </div>
+</div>
--- web/src/routes/commission/[id]/+layout.ts
+++ web/src/routes/commission/[id]/+layout.ts
@@ -1,18 +1,35 @@
 import type {Commission} from "./commission_types";
 import {selectedProblem} from "$lib/store";
+import {get} from "svelte/store";
+import {error} from "@sveltejs/kit";
 
 export async function load({fetch, params}) {
     console.log("1", params.id);
 
-    let commission: Commission = await fetch(`http://localhost:5000/commission/${params.id}`)
-        .then(response => response.json());
+    // We load the problem here to avoid the need to load it in the layout.
+    // This way we can ensure that the problem is loaded before the layout is rendered.
+    // This simplifies the layout code a lot.
+    let response = await fetch(`http://localhost:5000/commission/${params.id}`);
 
-    selectedProblem.update((p) => {
-        if(p === undefined)
-            return commission;
-        else
-            return p;
-    })
+    if (!response.ok) {
+        switch (response.status) {
+            case 404: {
+                throw error(404,
+                    `La commissione con ID ${params.id} non esiste. 
+                     Dettagli: ${await response.text()}`
+                );
+            }
+            default: {
+                // @ts-ignore
+                throw error(response.status, await response.text());
+            }
+        }
+    }
+
+    let commission: Commission = await response.json()
+
+    if (get(selectedProblem) === undefined)
+        selectedProblem.set(commission);
 
     return {
         'commission': commission,
