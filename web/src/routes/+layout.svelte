b21775206a516c8890fe5f17c86b141bc5d8ab32
Stefano Vittorio Porta <stefa168@hotmail.it>
2024-03-22 01:01:46 +0100

    feat(settings): Implemented settings dialog

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import "../app.css";
-    import {ModeWatcher} from "mode-watcher";
+    import {ModeWatcher, userPrefersMode} from "mode-watcher";
     import {toast} from "svelte-sonner";
     import {goto, beforeNavigate, afterNavigate} from "$app/navigation";
     import {onMount} from "svelte";
@@ -20,6 +20,9 @@
     import NewCommissionDialog from "$lib/NewCommissionDialog.svelte";
     import * as ContextMenu from "$lib/components/ui/context-menu"
     import * as AlertDialog from "$lib/components/ui/alert-dialog";
+    import * as Dialog from "$lib/components/ui/dialog";
+    import * as Switch from "$lib/components/ui/switch";
+    import * as Select from "$lib/components/ui/select";
     import {Button} from "$lib/components/ui/button";
     import {Toaster} from "$lib/components/ui/sonner";
     import type {Commission} from "./commission/[id]/commission_types";
@@ -28,8 +31,11 @@
         commissionsPreviewLoaded,
         commissionsPreview,
         fetchCommissionPreviews,
-        deleteCommission
+        deleteCommission, debugEnabled
     } from "$lib/store";
+    import {Separator} from "$lib/components/ui/separator";
+    import type {Selected} from "bits-ui";
+    import {get} from "svelte/store";
 
     onMount(async () => {
         await fetchCommissionPreviews();
@@ -61,6 +67,19 @@
             });
         }
     }
+
+    let settingsPopoverOpen = false;
+    const handleThemeChange = (value: Selected<string> | undefined) => {
+        const theme = value?.value;
+        if (theme !== "system" && theme !== "light" && theme !== "dark") return;
+        userPrefersMode.set(theme);
+    }
+
+    const themes: Selected<string>[] = [
+        {value: "light", label: "Chiaro"},
+        {value: "dark", label: "Scuro"},
+        {value: "system", label: "Sistema"}
+    ];
 </script>
 
 <ModeWatcher/>
@@ -163,7 +182,7 @@
     </nav>
     <footer id="sidebar-footer"
             class="flex-shrink-0 p-4 w-full z-60 bottom-0 start-0 pt-4 border-t">
-        <ul class="ps-0.5 space-y-0.5 text-md text-gray-400 dark:text-gray-500">
+        <ul class="ps-0.5 space-y-0.5 text-md text-gray-600 dark:text-gray-500">
             <li>
                 <button class="flex items-center transition dark:hover:text-white">
                     <MdiRobotExcited/>
@@ -171,10 +190,62 @@
                 </button>
             </li>
             <li>
-                <button class="flex items-center transition dark:hover:text-white">
+                <button class="flex items-center transition dark:hover:text-white"
+                        on:click={() => settingsPopoverOpen = !settingsPopoverOpen}>
                     <MdiCogOutline/>
                     <span class="ms-1.5">Impostazioni</span>
                 </button>
+                <Dialog.Root bind:open={settingsPopoverOpen}>
+                    <Dialog.Content>
+                        <Dialog.Header class="mb-2">
+                            <Dialog.Title>
+                                Impostazioni dell'Interfaccia
+                            </Dialog.Title>
+                        </Dialog.Header>
+                        <div class="flex flex-row items-center justify-between px-4">
+                            <div class="space-y-0.5">
+                                <div class="flex items-center justify-between">
+                                    <div class="text-sm font-medium leading-none mb-2">
+                                        Tema
+                                    </div>
+                                    <Select.Root
+                                            onSelectedChange={handleThemeChange}
+                                            selected={themes.find(t => t.value === get(userPrefersMode))}>
+                                        <Select.Trigger class="w-[180px]">
+                                            <Select.Value placeholder="Tema"/>
+                                        </Select.Trigger>
+                                        <Select.Content>
+                                            {#each themes as theme (theme.value)}
+                                                <Select.Item value={theme.value}>
+                                                    {theme.label}
+                                                </Select.Item>
+                                            {/each}
+                                        </Select.Content>
+                                    </Select.Root>
+                                </div>
+                                <div class="text-[0.8rem] text-muted-foreground mt-2">
+                                    È possibile scegliere tra un'interfaccia chiara e una scura. In alternativa, è
+                                    possibile lasciare che il sistema scelga automaticamente in base alle impostazioni
+                                    del sistema operativo.
+                                </div>
+                            </div>
+
+                        </div>
+                        <Separator decorative={true}/>
+                        <div class="flex flex-row items-center justify-between px-4">
+                            <div class="space-y-0.5">
+                                <div class="text-sm font-medium leading-none mb-2">
+                                    Debug
+                                </div>
+                                <div class="text-[0.8rem] text-muted-foreground">
+                                    Attivando questa impostazione verranno mostrati, dove predisposti, una serie di dati
+                                    aggiuntivi utili per lo sviluppo.
+                                </div>
+                            </div>
+                            <Switch.Root bind:checked={$debugEnabled}/>
+                        </div>
+                    </Dialog.Content>
+                </Dialog.Root>
             </li>
             <li>
                 <button class="flex items-center transition dark:hover:text-white">
