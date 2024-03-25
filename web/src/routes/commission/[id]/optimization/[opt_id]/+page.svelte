b4c326f938c5e5d5725769bbd33ed5658ba60949
Stefano Vittorio Porta <stefa168@hotmail.it>
2024-03-25 23:09:18 +0100

    chore: moved schema to separate file

--- web/src/routes/commission/[id]/optimization/[opt_id]/+page.svelte
+++ web/src/routes/commission/[id]/optimization/[opt_id]/+page.svelte
@@ -11,9 +11,8 @@
     import * as Form from "$lib/components/ui/form";
     import * as HoverCard from "$lib/components/ui/hover-card";
 
-    import SuperDebug, {defaults, superForm, superValidate} from "sveltekit-superforms";
+    import SuperDebug, {defaults, superForm} from "sveltekit-superforms";
     import {zod} from "sveltekit-superforms/adapters";
-    import {z} from "zod";
 
     import {SolverType} from "../optimization_types";
     import MdiReminder from '~icons/mdi/reminder'
@@ -21,61 +20,7 @@
     import MdiCogPlayOutline from '~icons/mdi/cog-play-outline'
     import MdiUndoVariant from '~icons/mdi/undo-variant'
     import MdiCloudArrowUp from '~icons/mdi/cloud-arrow-up'
-    import MdiPlayOutline from '~icons/mdi/play-outline'
-
-    const optimizationConfigurationSchema = z.object({
-        title: z.string().min(1).max(256),
-        max_duration: z.coerce.number().min(0).default(210),
-        max_commissions_morning: z.coerce.number().min(0).default(6),
-        max_commissions_afternoon: z.coerce.number().min(0).default(6),
-        online: z.boolean().default(false),
-
-        min_professor_number: z.coerce.number().min(1).nullable().default(null),
-        min_professor_number_masters: z.coerce.number().min(1).nullable().default(null),
-        max_professor_number: z.coerce.number().min(1).nullable().default(null),
-
-        solver: z.nativeEnum(SolverType).default(SolverType.CPLEX),
-        optimization_time_limit: z.coerce.number().min(60).default(60),
-        optimization_gap: z.coerce.number().min(0).default(0.005),
-    }).refine((data) => {
-        // If online, then the minimum number of professors must be defined
-        return data.online ? data.min_professor_number !== null : true;
-    }, {
-        message: "Il numero minimo di professori deve essere definito se la commissione è online",
-        path: ["min_professor_number"]
-    }).refine((data) => {
-        // If online, then the minimum number of professors for the master's degree must be defined
-        return data.online ? data.min_professor_number_masters !== null : true;
-    }, {
-        message: "Il numero minimo di professori per il corso di laurea magistrale deve essere definito se la commissione è online",
-        path: ["min_professor_number_masters"]
-    }).refine((data) => {
-        // If online, then the maximum number of professors must be defined
-        return data.online ? data.max_professor_number !== null : true;
-    }, {
-        message: "Il numero massimo di professori deve essere definito se la commissione è online",
-        path: ["max_professor_number"]
-    }).refine((data) => {
-        // If online, then the minimum number of professors must be less than or equal to the maximum number of professors
-        if (data.online && data.min_professor_number !== null && data.max_professor_number !== null) {
-            return data.min_professor_number <= data.max_professor_number;
-        } else {
-            return true;
-        }
-    }, {
-        message: "Il numero minimo di professori deve essere minore o uguale al numero massimo di professori se la commissione è online",
-        path: ["min_professor_number", "max_professor_number"]
-    }).refine((data) => {
-        // If online, then the minimum number of professors must be less than or equal to the maximum number of professors
-        if (data.online && data.min_professor_number !== null && data.max_professor_number !== null) {
-            return data.min_professor_number <= data.max_professor_number;
-        } else {
-            return true;
-        }
-    }, {
-        message: "Il numero minimo di professori deve essere minore o uguale al numero massimo di professori se la commissione è online",
-        path: ["min_professor_number_masters", "max_professor_number"]
-    });
+    import {optimizationConfigurationSchema} from "./schema";
 
     const form = superForm(
         defaults($selectedConfiguration, zod(optimizationConfigurationSchema)), {
---
+++ web/src/routes/commission/[id]/optimization/[opt_id]/schema.ts
@@ -0,0 +1,56 @@
+import {z} from "zod";
+import {SolverType} from "../optimization_types";
+
+export const optimizationConfigurationSchema = z.object({
+    title: z.string().min(1).max(256),
+    max_duration: z.coerce.number().min(0).default(210),
+    max_commissions_morning: z.coerce.number().min(0).default(6),
+    max_commissions_afternoon: z.coerce.number().min(0).default(6),
+    online: z.boolean().default(false),
+
+    min_professor_number: z.coerce.number().min(1).nullable().default(null),
+    min_professor_number_masters: z.coerce.number().min(1).nullable().default(null),
+    max_professor_number: z.coerce.number().min(1).nullable().default(null),
+
+    solver: z.nativeEnum(SolverType).default(SolverType.CPLEX),
+    optimization_time_limit: z.coerce.number().min(60).default(60),
+    optimization_gap: z.coerce.number().min(0).default(0.005),
+}).refine((data) => {
+    // If online, then the minimum number of professors must be defined
+    return data.online ? data.min_professor_number !== null : true;
+}, {
+    message: "Il numero minimo di professori deve essere definito se la commissione è online",
+    path: ["min_professor_number"]
+}).refine((data) => {
+    // If online, then the minimum number of professors for the master's degree must be defined
+    return data.online ? data.min_professor_number_masters !== null : true;
+}, {
+    message: "Il numero minimo di professori per il corso di laurea magistrale deve essere definito se la commissione è online",
+    path: ["min_professor_number_masters"]
+}).refine((data) => {
+    // If online, then the maximum number of professors must be defined
+    return data.online ? data.max_professor_number !== null : true;
+}, {
+    message: "Il numero massimo di professori deve essere definito se la commissione è online",
+    path: ["max_professor_number"]
+}).refine((data) => {
+    // If online, then the minimum number of professors must be less than or equal to the maximum number of professors
+    if (data.online && data.min_professor_number !== null && data.max_professor_number !== null) {
+        return data.min_professor_number <= data.max_professor_number;
+    } else {
+        return true;
+    }
+}, {
+    message: "Il numero minimo di professori deve essere minore o uguale al numero massimo di professori se la commissione è online",
+    path: ["min_professor_number", "max_professor_number"]
+}).refine((data) => {
+    // If online, then the minimum number of professors must be less than or equal to the maximum number of professors
+    if (data.online && data.min_professor_number !== null && data.max_professor_number !== null) {
+        return data.min_professor_number <= data.max_professor_number;
+    } else {
+        return true;
+    }
+}, {
+    message: "Il numero minimo di professori deve essere minore o uguale al numero massimo di professori se la commissione è online",
+    path: ["min_professor_number_masters", "max_professor_number"]
+});
