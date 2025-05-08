6dd3fd203a021d467712cd8849c3ed2ba361114f
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-05-08 17:58:53 +0200

    refactor: configured logging

--- server/v2/asgi.py
+++ server/v2/asgi.py
@@ -1,6 +1,9 @@
-from dataclasses import dataclass
+import logging
+import sys
+from functools import lru_cache
 from typing import Any
 
+import structlog
 from litestar import Litestar, get
 from litestar.serialization import decode_json, encode_json
 from litestar.plugins.sqlalchemy import (
@@ -9,6 +12,15 @@ from litestar.plugins.sqlalchemy import (
     AsyncSessionConfig,
     AlembicAsyncConfig
 )
+from litestar.middleware.logging import LoggingMiddlewareConfig
+from litestar.logging.config import (
+    LoggingConfig,
+    StructLoggingConfig,
+    default_logger_factory,
+    default_structlog_processors,
+    default_structlog_standard_lib_processors,
+)
+from litestar.plugins.structlog import StructlogConfig, StructlogPlugin
 from pydantic import BaseModel, PostgresDsn
 from sqlalchemy import event
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
@@ -118,10 +130,123 @@ class DatabaseSettings(BaseModel):
         return self._engine_instance
 
 
-def create_app() -> Litestar:
-    db_settings = DatabaseSettings(url=PostgresDsn("postgresql+psycopg://user:password@localhost:5432/postgres"))
+class LogSettings(BaseModel):
+    """Logger configuration"""
 
-    alchemy = SQLAlchemyPlugin(config=SQLAlchemyAsyncConfig(
+    exclude_paths: str = r"\A(?!x)x"
+    """Regex to exclude paths from logging."""
+
+    http_event: str = "HTTP"
+    """Log event name for logs from Litestar handlers."""
+
+    include_compressed_body: bool = False
+    """Include 'body' of compressed responses in log output."""
+
+    level: int = 30
+    """Stdlib log levels. Only emit logs at this level, or higher."""
+
+    obfuscate_cookies: set[str] = {"session", "XSRF-TOKEN"}
+    """Request cookie keys to obfuscate."""
+
+    obfuscate_headers: set[str] = {"Authorization", "X-API-KEY", "X-XSRF-TOKEN"}
+    """Request header keys to obfuscate."""
+
+    job_fields: list[str] = [
+        "function",
+        "kwargs",
+        "key",
+        "scheduled",
+        "attempts",
+        "completed",
+        "queued",
+        "started",
+        "result",
+        "error",
+    ]
+    """Attributes of the SAQ Job to be logged."""
+
+    request_fields: list[str] = [
+        "path",
+        "method",
+        "query",
+        "path_params",
+    ]
+    """Attributes of the Request to be logged."""
+
+    response_fields: list[str] = ["status_code"]
+    """Attributes of the Response to be logged."""
+
+    worker_event: str = "Worker"
+    """Log event name for logs from SAQ worker."""
+
+    saq_level: int = 50
+    """Level to log SAQ logs."""
+
+    sqlalchemy_level: int = 30
+    """Level to log SQLAlchemy logs."""
+
+    asgi_access_level: int = 30
+    """Level to log uvicorn access logs."""
+
+    asgi_error_level: int = 30
+    """Level to log uvicorn error logs."""
+
+
+@lru_cache
+def _is_tty() -> bool:
+    return bool(sys.stderr.isatty() or sys.stdout.isatty())
+
+
+def configure_logging(settings: LogSettings) -> StructlogConfig:
+    _render_as_json = not _is_tty()
+    _structlog_default_processors = default_structlog_processors(as_json=_render_as_json)
+    _structlog_default_processors.insert(1, structlog.processors.EventRenamer("message"))
+    _structlog_standard_lib_processors = default_structlog_standard_lib_processors(as_json=_render_as_json)
+    _structlog_standard_lib_processors.insert(1, structlog.processors.EventRenamer("message"))
+
+    return StructlogConfig(
+        structlog_logging_config=StructLoggingConfig(
+            log_exceptions="always",
+            processors=_structlog_default_processors,
+            logger_factory=default_logger_factory(as_json=_render_as_json),
+            standard_lib_logging_config=LoggingConfig(
+                root={"level": logging.getLevelName(settings.level), "handlers": ["queue_listener"]},
+                formatters={
+                    "standard": {
+                        "()": structlog.stdlib.ProcessorFormatter,
+                        "processors": _structlog_standard_lib_processors,
+                    },
+                },
+                loggers={
+                    "sqlalchemy.engine": {
+                        "propagate": False,
+                        "level": settings.sqlalchemy_level,
+                        "handlers": ["queue_listener"],
+                    },
+                    "sqlalchemy.pool": {
+                        "propagate": False,
+                        "level": settings.sqlalchemy_level,
+                        "handlers": ["queue_listener"],
+                    },
+                },
+            ),
+        ),
+        middleware_logging_config=LoggingMiddlewareConfig(
+            request_log_fields=settings.request_fields,
+            response_log_fields=settings.response_fields,
+        ),
+    )
+
+
+def create_app() -> Litestar:
+    # settings = load_settings(settings_path)
+
+    db_settings = DatabaseSettings(url=PostgresDsn("postgresql+psycopg://user:password@localhost:5432/postgres"))
+    log_settings = LogSettings()
+
+    structlog_plugin = StructlogPlugin(config=configure_logging(log_settings))
+
+    alchemy_plugin = SQLAlchemyPlugin(config=SQLAlchemyAsyncConfig(
         engine_instance=db_settings.get_engine(),
         before_send_handler="autocommit",
         session_config=AsyncSessionConfig(expire_on_commit=False),
@@ -134,7 +259,7 @@ def create_app() -> Litestar:
 
     app = Litestar(
         route_handlers=[hello_world],
-        plugins=[alchemy]
+        plugins=[structlog_plugin, alchemy_plugin]
     )
 
     return app
