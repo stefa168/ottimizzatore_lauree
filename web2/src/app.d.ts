80f3ad4be23a883231c1496acb46275d07014cb1
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-06-12 13:09:59 +0200

    fix: fixed icon import

--- web2/src/app.d.ts
+++ web2/src/app.d.ts
@@ -1,3 +1,6 @@
+// Fix for modules not being found https://github.com/unplugin/unplugin-icons?tab=readme-ov-file#configuration
+import 'unplugin-icons/types/svelte'
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
--- web2/src/routes/+page.svelte
+++ web2/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import MdiRobotConfusedOutline from '~icons/mdi/robot-confused-outline'
+</script>
+
+<div class="mt-32 flex flex-col items-center justify-center h-full">
+    <MdiRobotConfusedOutline class="w-16 h-16"/>
+    <div class="mt-4 text-center text-lg font-medium text-gray-600 dark:text-gray-400">
+        <p>Non hai aperto alcuna Sessione di Laurea.</p>
+        <p>Per iniziare, scegline o creane una usando il menù laterale.</p>
+    </div>
+</div>
