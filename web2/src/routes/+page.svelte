09118edc2f307d3269f917b2136a9ec43a37ef9b
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-06-27 22:30:17 +0200

    feat: implemented main page links for session upload

--- web2/src/routes/+page.svelte
+++ web2/src/routes/+page.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
-    import MdiRobotConfusedOutline from '~icons/mdi/robot-confused-outline'
+  import MdiRobotConfusedOutline from '~icons/mdi/robot-confused-outline'
+  import {Button} from "@/components/ui/button";
 </script>
 
 <div class="flex flex-col items-center justify-center h-full">
-    <MdiRobotConfusedOutline class="w-16 h-16"/>
-    <div class="mt-4 text-center text-lg font-medium text-gray-600 dark:text-gray-400">
-        <p>Non hai aperto alcuna Sessione di Laurea.</p>
-        <p>Per iniziare, scegline o creane una usando il menù laterale.</p>
-    </div>
+  <MdiRobotConfusedOutline class="w-16 h-16"/>
+  <div class="mt-4 text-center text-lg font-medium text-gray-600 dark:text-gray-400">
+    <p>Non hai aperto alcuna Sessione di Laurea.</p>
+    <p>Puoi
+      <Button variant="outline" href="/gradsession">Aprirne</Button>
+      una esistente, o
+      <Button variant="outline" href="gradsession/new">Crearne</Button>
+      una nuova.
+    </p>
+  </div>
 </div>
