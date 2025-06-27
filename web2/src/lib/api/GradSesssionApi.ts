e89a03da1ffaf43bb885878f4f0fc6dbb5a3d1c5
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-06-27 22:26:36 +0200

    feat: implemented mutation for spreadsheet upload

--- web2/src/lib/api/GradSesssionApi.ts
+++ web2/src/lib/api/GradSesssionApi.ts
@@ -1,9 +1,25 @@
 // todo replace
-import type {GradSession} from "@/types";
-import {createQuery, type QueryClient} from "@tanstack/svelte-query";
+import type {ApiErrorResponse, GradSession} from "@/types";
+import {createMutation, createQuery, type QueryClient} from "@tanstack/svelte-query";
+import type {CommissionFormData, UploadErrorDetails} from "@/schema/CommissionFormSchema";
 
 const PUBLIC_BACKEND_URL = "http://127.0.0.1:8000/api/v1";
+
 // import { PUBLIC_BACKEND_URL } from '$env/static/public';
+
+export interface RawGradSession {
+  id: number;
+  title: string;
+  created_at: string;
+  updated_at: string;
+}
+
+// transforms.ts
+export const transformGradSession = (raw: RawGradSession): GradSession => ({
+  ...raw,
+  created_at: new Date(raw.created_at),
+  updated_at: new Date(raw.updated_at)
+});
 
 export const GradSessionKeys = {
   all: ['gs'] as const,
@@ -11,13 +27,31 @@ export const GradSessionKeys = {
 }
 
 export const GradSessionApi = (customFetch = fetch) => ({
-  getAll: async (): Promise<GradSession[]> => {
+  getAll: async (): Promise<RawGradSession[]> => {
     const response = await customFetch(`${PUBLIC_BACKEND_URL}/sessions`);
-    return (await response.json()) as GradSession[]
+    return (await response.json()) as RawGradSession[]
   },
-  getById: async (id: number): Promise<GradSession> => {
+  getById: async (id: number): Promise<RawGradSession> => {
     const response = await customFetch(`${PUBLIC_BACKEND_URL}/sessions/${id}`);
-    return (await response.json()) as GradSession;
+    return (await response.json()) as RawGradSession;
+  },
+  create: async (data: CommissionFormData): Promise<RawGradSession> => {
+    const formData = new FormData();
+    // The not null check should never happen: we have a LOT of checks in place before arriving here, all set up with
+    // zod that validates the passed data for us.
+    formData.append('file', data.excel!);
+    formData.append('title', data.title);
+
+    const response = await customFetch(`${PUBLIC_BACKEND_URL}/sessions/upload`, {
+      method: 'POST',
+      body: formData
+    });
+
+    if (!response.ok) {
+      throw await response.json();
+    }
+
+    return await response.json() as Promise<RawGradSession>
   }
 });
 
@@ -26,10 +60,16 @@ export const GradSessionApiQueries = (queryClient: QueryClient, customFetch = fe
     createQuery({
       queryKey: GradSessionKeys.all,
       queryFn: GradSessionApi(customFetch).getAll,
-      select: data => data.map(session => ({
-        ...session,
-        created_at: new Date(session.created_at),
-        updated_at: new Date(session.updated_at)
-      }))
+      select: data => data.map(transformGradSession)
+    }),
+  uploadSessionMutation: () =>
+    createMutation<GradSession, ApiErrorResponse<UploadErrorDetails>, CommissionFormData>({
+      mutationFn: (data: CommissionFormData) => GradSessionApi(customFetch)
+        .create(data)
+        .then((r) => transformGradSession(r)),
+      onSuccess: (raw) => {
+        queryClient.invalidateQueries({queryKey: GradSessionKeys.all});
+      },
+
     }),
 });
