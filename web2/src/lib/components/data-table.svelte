2f554ce8fb8066de62ba1ace4c879502b79368a5
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-07-02 13:19:35 +0200

    style: indentation

--- web2/src/lib/components/data-table.svelte
+++ web2/src/lib/components/data-table.svelte
@@ -1,61 +1,61 @@
 <script lang="ts" generics="TData, TValue">
-    import {type ColumnDef, getCoreRowModel} from "@tanstack/table-core";
-    import {createSvelteTable, FlexRender} from "@/components/ui/data-table";
-    import * as Table from "@/components/ui/table";
+  import {type ColumnDef, getCoreRowModel} from "@tanstack/table-core";
+  import {createSvelteTable, FlexRender} from "@/components/ui/data-table";
+  import * as Table from "@/components/ui/table";
 
-    type DataTableProps<TData, TValue> = {
-        columns: ColumnDef<TData, TValue>[];
-        data: TData[];
-    };
+  type DataTableProps<TData, TValue> = {
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+  };
 
-    let {data, columns}: DataTableProps<TData, TValue> = $props();
+  let {data, columns}: DataTableProps<TData, TValue> = $props();
 
-    const table = createSvelteTable({
-        get data() {
-            return data;
-        },
-        columns,
-        getCoreRowModel: getCoreRowModel()
-    })
+  const table = createSvelteTable({
+    get data() {
+      return data;
+    },
+    columns,
+    getCoreRowModel: getCoreRowModel()
+  })
 </script>
 
 <div class="rounded-md border">
-    <Table.Root>
-        <Table.Header>
-            {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
-                <Table.Row>
-                    {#each headerGroup.headers as header (header.id)}
-                        <Table.Head>
-                            {#if !header.isPlaceholder}
-                                <FlexRender
-                                        content={header.column.columnDef.header}
-                                        context={header.getContext()}
-                                />
-                            {/if}
-                        </Table.Head>
-                    {/each}
-                </Table.Row>
-            {/each}
-        </Table.Header>
-        <Table.Body>
-            {#each table.getRowModel().rows as row (row.id)}
-                <Table.Row data-state={row.getIsSelected() && "selected"}>
-                    {#each row.getVisibleCells() as cell (cell.id)}
-                        <Table.Cell>
-                            <FlexRender
-                                    content={cell.column.columnDef.cell}
-                                    context={cell.getContext()}
-                            />
-                        </Table.Cell>
-                    {/each}
-                </Table.Row>
-            {:else}
-                <Table.Row>
-                    <Table.Cell colspan={columns.length} class="h-24 text-center">
-                        No results.
-                    </Table.Cell>
-                </Table.Row>
-            {/each}
-        </Table.Body>
-    </Table.Root>
+  <Table.Root>
+    <Table.Header>
+      {#each table.getHeaderGroups() as headerGroup (headerGroup.id)}
+        <Table.Row>
+          {#each headerGroup.headers as header (header.id)}
+            <Table.Head>
+              {#if !header.isPlaceholder}
+                <FlexRender
+                    content={header.column.columnDef.header}
+                    context={header.getContext()}
+                />
+              {/if}
+            </Table.Head>
+          {/each}
+        </Table.Row>
+      {/each}
+    </Table.Header>
+    <Table.Body>
+      {#each table.getRowModel().rows as row (row.id)}
+        <Table.Row data-state={row.getIsSelected() && "selected"}>
+          {#each row.getVisibleCells() as cell (cell.id)}
+            <Table.Cell>
+              <FlexRender
+                  content={cell.column.columnDef.cell}
+                  context={cell.getContext()}
+              />
+            </Table.Cell>
+          {/each}
+        </Table.Row>
+      {:else}
+        <Table.Row>
+          <Table.Cell colspan={columns.length} class="h-24 text-center">
+            No results.
+          </Table.Cell>
+        </Table.Row>
+      {/each}
+    </Table.Body>
+  </Table.Root>
 </div>
