465ba7a3497ac4aae2ea75f86b3c0e450093ecb5
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-07-05 17:41:14 +0200

    chore: cleanup

--- web2/src/routes/gradsession/[id]/+layout.ts
+++ web2/src/routes/gradsession/[id]/+layout.ts
@@ -3,7 +3,6 @@ import {GradSessionApi} from "@/api/GradSesssionApi";
 import {transformGradSession, transformSessionProfessorList} from "@/api/RawTypes";
 import {error} from "@sveltejs/kit";
 import type {ApiErrorResponse} from "@/types";
-import {computeProfessorsBurdens} from "@/utils";
 
 export const load: LayoutLoad = async ({params, parent, fetch}) => {
   const session_id = Number.parseInt(params.id);
@@ -16,15 +15,11 @@ export const load: LayoutLoad = async ({params, parent, fetch}) => {
       api.getProfessors(session_id).then(transformSessionProfessorList)
     ]);
 
-    let professorsMap = new Map(professors.map(p => [p.id, p]));
-
     return {
       session_id: session_id,
       session,
       student_entries,
       professors,
-      professorsMap,
-      professorsBurdens: computeProfessorsBurdens(professorsMap, student_entries)
     };
   } catch (err) {
     // Handle the API error appropriately
