76e2026a865f69036965cd788b5a7ba2a78e3f7a
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-07-06 22:15:09 +0200

    fix: fixed double import

--- web2/src/routes/gradsession/[id]/professors/+page.svelte
+++ web2/src/routes/gradsession/[id]/professors/+page.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
   import {columns} from "./columns";
   import DataTable from "@/components/data-table.svelte";
-  import {columns} from "./columns";
   import {getSessionData} from "../../SessionData.svelte";
 
   let sessionData = getSessionData();
-
-  $inspect(data.professorsBurdens);
 </script>
 
-<DataTable data={data.professors} columns={columns(data.professorsBurdens)}/>
 <DataTable data={sessionData.professors} columns={columns(sessionData)}/>
