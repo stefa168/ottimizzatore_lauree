421ee51054b83cbee050dbf6b69ebf1a5199905c
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-08-17 20:10:14 +0200

    feat: implemented xls inspection before upload

--- web2/src/routes/gradsession/new/+page.svelte
+++ web2/src/routes/gradsession/new/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import {defaults, fileProxy, superForm,} from "sveltekit-superforms";
   import {zod} from "sveltekit-superforms/adapters";
-  import {commissionFormSchema, type UploadErrorDetails} from "@/schema/CommissionFormSchema";
+  import * as XLSX from 'xlsx';
+  import {commissionFormSchema} from "@/schema/CommissionFormSchema";
   import {useQueryClient} from "@tanstack/svelte-query";
   import {GradSessionApiQueries} from "@/api/GradSesssionApi";
 
@@ -18,8 +19,13 @@
 
   // Icons
   import MdiAlertOutline from '~icons/mdi/alert-outline'
+
+  import type {PageProps} from './$types';
   import {goto, invalidate} from "$app/navigation";
   import {toast} from "svelte-sonner";
+  import Inspect from "svelte-inspect-value";
+
+  let {data}: PageProps = $props();
 
   // TSQ
   const queryClient = useQueryClient();
@@ -55,7 +61,44 @@
 
   let upload_error = $derived($uploadNewGSMutation.error)
   let submitting = $derived($uploadNewGSMutation.isPending)
+
+  let excelRows = $derived.by(async () => {
+    const f = $file[0];
+    if (!f) return;
+
+    // Parse the workbook
+    const data = await f.arrayBuffer();
+    const wb = XLSX.read(data, {type: 'array'});
+
+    // Use the first sheet
+    const sheetName = wb.SheetNames[0];
+    const ws = wb.Sheets[sheetName];
+
+    return XLSX.utils.sheet_to_json(ws, {
+      header: 1,   // returns array-of-arrays
+      defval: ''   // keep empty cells as ''
+    }) as string[][];
+
+  });
+
+  let excelColumns = $derived.by(async () => {
+    let rows = (await excelRows)?.[0] ?? [];
+    return new Set((rows.map((h) => String(h).trim().toUpperCase())))
+  })
+
+  let missingColumns = $derived.by(async () => {
+    let columns = await excelColumns;
+    if (columns.size <= 0) return new Set<string>();
+    return data.expectedColumns.difference(columns);
+  })
+
+  let xlsxPreview = $derived.by(async () => {
+    // const idxMap = data.expectedColumns.keys().map((c) => (await excelColumns).indexOf(c));
+    return (await excelRows)?.slice(1, 11).map(sl => sl.join(", ")) ?? []
+  })
 </script>
+
+<Inspect.Values {excelRows} {excelColumns} {missingColumns} {xlsxPreview}/>
 
 <div class="border-b-2 mb-6">
   <h2 class="text-3xl mt-4 mb-1"> Nuova Sessione di Laurea </h2>
@@ -71,8 +114,10 @@
     {#if upload_error}
       <Alert.Root variant='destructive' class="mb-4">
         <MdiAlertOutline class="me-2 h-4 w-4"/>
-        <Alert.Title class="mb-2"><p>Errore</p></Alert.Title>
+        <Alert.Title class="mb-2"><p>Errore (HTTP {upload_error.status_code})</p></Alert.Title>
         <Alert.Description>
+          <p>Il server ha restituito il seguente messaggio di errore:</p>
+          <p><code>{upload_error.detail}</code></p>
           {#if upload_error.extra?.details}
             <p class="font-mono text-xs">{upload_error.extra.details}</p>
           {/if}
@@ -112,6 +157,29 @@
       </Form.Description>
       <Form.FieldErrors/>
     </Form.Field>
-    <Form.Button>Invia</Form.Button>
+
+    {#await missingColumns}
+      <!-- Even though it isn't needed, we're using a copy of the button to avoid flashing and pops caused by the form updating -->
+      <Form.Button disabled>Invia</Form.Button>
+    {:then missing}
+      {#if missing.size > 0}
+        <Alert.Root variant='destructive' class="mb-4">
+          <MdiAlertOutline class="me-2 h-4 w-4"/>
+          <Alert.Title class="mb-2">Attenzione, il file è malformato; mancano le seguenti colonne richieste:
+          </Alert.Title>
+          <Alert.Description>
+            <ul class="list-inside list-disc">
+              {#each missing as el}
+                <li><code>{el}</code></li>
+              {/each}
+            </ul>
+          </Alert.Description>
+        </Alert.Root>
+      {/if}
+
+      <Form.Button disabled={(missing.size > 0)}>
+        Invia
+      </Form.Button>
+    {/await}
   </fieldset>
 </form>
