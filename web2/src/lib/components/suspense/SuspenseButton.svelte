e15434f3a0a11bef26e563e8cf23d3e4ebf7790f
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-08-27 15:21:28 +0200

    feat: added `class` prop to `SuspenseButton` and improved loading state handling

--- web2/src/lib/components/suspense/SuspenseButton.svelte
+++ web2/src/lib/components/suspense/SuspenseButton.svelte
@@ -1,27 +1,29 @@
 <script lang="ts">
   import {SuspenseManager} from "@/components/suspense/base.svelte";
   import type {Snippet} from "svelte";
+  import type {ClassValue} from "clsx";
 
   export type a = never;
 
   interface Props {
-    children: Snippet<[{
+    children?: Snippet<[{
       loading: boolean
     }]>;
-    onclick?: (event: MouseEvent) => Promise<void>
+    onclick?: (event: MouseEvent) => Promise<void>,
+    class?: ClassValue,
   }
 
-  let {children, onclick}: Props = $props();
+  let {children, onclick, class: className}: Props = $props();
 
   let manager = new SuspenseManager<unknown>();
   let loading = $derived(manager.running);
 
   export const clickEvent = async (event: MouseEvent) => {
+    loading = true;
     if (onclick) await manager.waitFor(onclick(event))
   }
 </script>
 
-<button onclick={clickEvent} disabled={(loading)}>
-  <!--{@render children({loading})}-->
-  {@render children({loading})}
+<button onclick={clickEvent} disabled={(loading)} class={[className]} data-loading={(loading)}>
+  {@render children?.({loading})}
 </button>
