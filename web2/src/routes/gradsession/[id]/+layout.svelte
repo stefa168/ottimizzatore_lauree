e887f6eb7b4e723b63fedee8e288ff161c7e0659
Stefano Vittorio Porta <stefa168@hotmail.it>
2025-08-30 12:44:52 +0200

    refactor: implemented current configuration tab

--- web2/src/routes/gradsession/[id]/+layout.svelte
+++ web2/src/routes/gradsession/[id]/+layout.svelte
@@ -3,29 +3,45 @@
   import NimbusUniversity from '~icons/nimbus/university'
   import MaterialSymbolsPersonPin from '~icons/material-symbols/person-pin'
   import MageRobotUwuFill from '~icons/mage/robot-uwu-fill'
+  import MdiCogOutline from '~icons/mdi/cog-outline'
 
   import type {Component} from "svelte";
   import type {SvelteHTMLElements} from "svelte/elements";
   import type {LayoutProps} from "./$types";
   import {page} from '$app/state';
   import {setSessionData} from "../SessionData.svelte";
-  import Inspect from "svelte-inspect-value";
-  import {debugEnabled} from "@/store.svelte";
   import EditableSessionTitle from "./EditableSessionTitle.svelte";
 
   let {children, data}: LayoutProps = $props();
   let sessionData = setSessionData(data.session, data.student_entries, data.professors);
 
-  type Section = { label: string, slug: string, path?: string, icon: Component<SvelteHTMLElements['svg']> };
+  type Section = { label: string, slug: string, path?: string, icon?: Component<SvelteHTMLElements['svg']> };
   const sections: Section[] = [
     {label: 'Informazioni', slug: 'info', path: '', icon: MdiInformationVariantBoxOutline},
     {label: 'Studenti Candidati', slug: 'candidates', icon: NimbusUniversity},
     {label: 'Docenti della Sessione', slug: 'professors', icon: MaterialSymbolsPersonPin},
     {label: 'Ottimizzazione', slug: 'optimization', icon: MageRobotUwuFill},
+    {label: 'Configurazione Aperta', slug: 'conf', icon: MdiCogOutline}
   ];
 
-  let currentSection = $derived(page.url.pathname.split('/').at(3) ?? 'info');
+  const currentSection = $derived(page.url.pathname.split('/').at(3) ?? 'info');
+  const isConfigOpen = $derived(page.url.pathname.split('/').at(4) !== undefined)
 </script>
+
+{#snippet tab(url: String | URL, section: Section, selected: Boolean)}
+  <li class={[section.slug === 'conf' && (isConfigOpen ? 'visible' : 'hidden')]}>
+    <a href={url.toString()}
+       class={[
+                 "inline-flex items-center justify-center p-4 px-2 border-b-2 border-transparent rounded-t-lg hover:cursor-pointer hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group transition-all ease-in-out duration-150",
+                 selected && '!text-primary !border-primary'
+               ]}>
+      {#if section.icon}
+        <section.icon class="w-4 h-4 me-2"/>
+      {/if}
+      <span>{section.label}</span>
+    </a>
+  </li>
+{/snippet}
 
 <div class="container mx-auto pb-10">
   <EditableSessionTitle {sessionData}/>
@@ -36,21 +52,15 @@
     <!-- Tabs List -->
     <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400">
       {#each sections as section}
-        <li>
-          <a href={`/gradsession/${data.session_id}/${section.path ?? section.slug}`}
-             data-active={currentSection === section.slug}
-             class="inline-flex items-center justify-center p-4 px-2 border-b-2 border-transparent rounded-t-lg hover:cursor-pointer hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300 group data-[active=true]:text-primary data-[active=true]:border-primary transition-all ease-in-out duration-150">
-            <section.icon class="w-4 h-4 me-2"/>
-            <span>{section.label}</span>
-          </a>
-        </li>
+        {@const
+          url = section.slug !== 'conf' ?
+          `/gradsession/${data.session_id}/${section.path ?? section.slug}` :
+          page.url
+        }
+        {@render tab(url, section, !isConfigOpen && currentSection === section.slug || isConfigOpen && section.slug === 'conf')}
       {/each}
     </ul>
   </div>
-
-  <!--{#if debugEnabled}-->
-  <!--  <Inspect value={sessionData} elementView="simple"/>-->
-  <!--{/if}-->
 
   {@render children?.()}
 </div>
